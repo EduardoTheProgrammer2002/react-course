6bb756e5c9fdabe311f31fddfc69fa8fa54c0730
Eduardo Rodriguez <edrodriguez@intellisys.com.do>
2022-03-09 11:33:48 -0800

    Card-list and Card classes converted into funcionals components.

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,23 +1,15 @@
-import { Component } from "react";
 import Card from "../card/card.component";
 import './card-list.styles.css'
 
 //this is the Card List component
-class CardList extends Component{
-    render() {
-        const { monsters } = this.props; //we stracted the value of props the we passed in app.js
-        return ( //then we just return the card component
-            <div className="card-list">
-                {
-                    monsters.map(monster => {
-                        return (
-                            <Card monster = {monster}  />
-                        )
-                    })
-                }
-            </div>       
-        );
-    }
+const CardList = ({ monsters }) => {
+    return ( //then we just return the card component
+        <div className="card-list">
+            {monsters.map(monster => {
+                return <Card monster = {monster}/>;
+            })}
+        </div>       
+    );
 }
 
 export default CardList;
