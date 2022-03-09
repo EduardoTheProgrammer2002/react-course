b986331b23127fe5396a5e066e4903d9c02f6bc4
Eduardo <edwardo16blak@gmail.com>
2022-03-09 08:23:43 -0400

    Monsters app with classes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+/* eslint-disable array-callback-return */
+import { Component } from 'react';
+import SearchBox from './components/search-box/search-box.component';
+import CardList from './components/card-list/card-list.component';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ''
+    }
+  }
+
+  // Using the user API
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(resp => resp.json())
+    .then(users => this.setState({ monsters: users }));
+  }
+
+  onSearchChange = (event) => {
+    //this stands for the string that the user puts on the search box
+    let searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => { // that way we update the searchfield to basicly pass it into the filter function of the monsters.
+      return { searchField };
+    })
+  }
+
+  render() {
+    //Ec2016 optimitation for confortable reable for us.
+    const {monsters, searchField} = this.state;
+    const { onSearchChange } = this;
+
+    //this is the array of monsters already filtered
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    return (
+      <div className="App">
+        <h1 className='app-title'>Monsters Rolodex</h1>
+        <SearchBox 
+        className = "monsters-search-box" 
+        placeholder = "Search monsters" 
+        onChangeHandler = {onSearchChange}/> 
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );  
+  }  
 }
 
 export default App;
